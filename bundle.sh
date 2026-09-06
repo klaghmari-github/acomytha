1d0c2aafdeb482fe276a73a6857cea3a57004ed1
laghmari khalil <laghmari.khalil@gmail.com>
2026-09-06 08:14:02 +0200

    checkpoint

--- bundle.sh
+++
@@ -1,5 +0,0 @@
-git fetch AkoMythaTTS-catalogue-tts.bundle \
-  feat/catalogue-tts-pipeline:feat/catalogue-tts-pipeline
-
-git checkout feat/catalogue-tts-pipeline
-git push -u origin feat/catalogue-tts-pipeline
